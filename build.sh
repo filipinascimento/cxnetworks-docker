dd7a6c178ca7d2251ed9710f8ba86106187195cf
Nascimento Silva <filsilva@iu.edu>
2021-03-12 10:32:22 -0500

    Added pyh5 package

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -x
 set -e
 
 docker build -t filsilva/cxnetwork .
-docker tag filsilva/cxnetwork filsilva/cxnetwork:0.2.0
+docker tag filsilva/cxnetwork filsilva/cxnetwork:0.2.2
 docker push filsilva/cxnetwork
