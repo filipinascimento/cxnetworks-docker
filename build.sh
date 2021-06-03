6a16425a3c8da9418c3d4a91e7fd3beb86ddf199
Filipi Nascimento Silva <filipinascimento@gmail.com>
2021-06-03 01:05:46 -0400

    Fixed version push script

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -x
 set -e
 
-docker build -t filsilva/cxnetwork .
-docker tag filsilva/cxnetwork filsilva/cxnetwork:0.2.2
-docker push filsilva/cxnetwork
+docker build -t filsilva/cxnetwork:0.2.3 -t filsilva/cxnetwork:0.2.3 .
+#docker tag filsilva/cxnetwork filsilva/cxnetwork:0.2.3
+docker push -a filsilva/cxnetwork
